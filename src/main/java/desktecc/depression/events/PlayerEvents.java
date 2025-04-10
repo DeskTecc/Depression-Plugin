32ebde64beb72a95c1fe413dcba512721f03f764
DeskTecc <techseven167@gmail.com>
2025-04-10 19:24:29 -0300

    Adding how mood can affect the game

--- src/main/java/desktecc/depression/events/PlayerEvents.java
+++ src/main/java/desktecc/depression/events/PlayerEvents.java
@@ -4,15 +4,31 @@ import desktecc.depression.Depression;
 import desktecc.depression.datas.PlayerMoodDATA;
 import desktecc.depression.datas.PlayerSleepDATA;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.Statistic;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scheduler.BukkitTask;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 import static desktecc.depression.Depression.*;
 
@@ -21,6 +37,71 @@ public class PlayerEvents implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        scheduler.runTaskTimer(Depression.getPlugin(), () -> {
+            if(getPlayerMental(player).getMentalPoints()<=70){
+                player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 15,1));
+            }
+            if(getPlayerMental(player).getMentalPoints()<=50){
+                player.addPotionEffect(new PotionEffect(PotionEffectType.MINING_FATIGUE, 15,1));
+            }
+            if(getPlayerMental(player).getMentalPoints()<=40){
+                Random rng = new Random();
+                int random_number = rng.nextInt(0,100);
+                if(random_number<=10){
+                    List<Sound> monster_sounds = Arrays.asList(
+                            Sound.ENTITY_CREEPER_HURT,
+                            Sound.ENTITY_CREEPER_DEATH,
+                            Sound.ENTITY_CREEPER_PRIMED,
+                            Sound.ENTITY_ENDERMAN_AMBIENT,
+                            Sound.ENTITY_ENDERMAN_SCREAM,
+                            Sound.ENTITY_ENDERMAN_STARE,
+                            Sound.ENTITY_ENDERMAN_DEATH,
+                            Sound.ENTITY_GHAST_SCREAM,
+                            Sound.ENTITY_GHAST_AMBIENT,
+                            Sound.ENTITY_GHAST_DEATH,
+                            Sound.ENTITY_SKELETON_AMBIENT,
+                            Sound.ENTITY_SKELETON_DEATH,
+                            Sound.ENTITY_SKELETON_STEP,
+                            Sound.ENTITY_SKELETON_HURT,
+                            Sound.ENTITY_WITHER_SKELETON_AMBIENT,
+                            Sound.ENTITY_WITHER_SKELETON_STEP,
+                            Sound.ENTITY_WITHER_SKELETON_HURT,
+                            Sound.ENTITY_SLIME_SQUISH,
+                            Sound.ENTITY_SPIDER_AMBIENT,
+                            Sound.ENTITY_SPIDER_HURT,
+                            Sound.ENTITY_SPIDER_STEP,
+                            Sound.ENTITY_SPIDER_DEATH,
+                            Sound.ENTITY_WARDEN_AMBIENT,
+                            Sound.ENTITY_WARDEN_DIG,
+                            Sound.ENTITY_WARDEN_DEATH,
+                            Sound.ENTITY_WITCH_AMBIENT,
+                            Sound.ENTITY_WITCH_DRINK,
+                            Sound.ENTITY_WITCH_HURT,
+                            Sound.ENTITY_WITCH_DEATH,
+                            Sound.ENTITY_ZOMBIE_AMBIENT,
+                            Sound.ENTITY_ZOMBIE_HURT,
+                            Sound.ENTITY_ZOMBIE_DEATH,
+                            Sound.ENTITY_ZOMBIE_STEP
+                            );
+                    Sound random_sound = monster_sounds.get(rng.nextInt(0, monster_sounds.size()));
+
+                    float random_volume = rng.nextFloat(1F,3F);
+
+                    player.playSound(player.getLocation(), random_sound, random_volume, 1F);
+                }
+            }
+            if(getPlayerMental(player).getMentalPoints()<=30){
+                if(getPlayerMental(player).getMentalPoints()<=10){
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 15, 1));
+                }else {
+                    if (player.getLocation().getBlock().getLightLevel() > 7) {
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 15, 1));
+                    }
+                }
+            }
+        }, 20L * 10L, 20L * 10L);
+
 
         //task for check sleep
         BukkitTask sleepCheck = new BukkitRunnable(){
@@ -66,5 +147,90 @@ public class PlayerEvents implements Listener {
         getPlayerMental(player).setNearEnderman(false);
         getPlayerMental(player).setOnDark(false);
         getPlayerMental(player).setVillagerClickCounter(0);
+    }
+
+    //mentalPoints below 90
+    @EventHandler
+    public void onPlayerEat(PlayerItemConsumeEvent event){
+
+        Player player = event.getPlayer();
+        if(getPlayerMental(player).getMentalPoints()<=90) {
+            List<Material> meat = Arrays.asList(
+                    Material.BEEF,
+                    Material.CHICKEN,
+                    Material.MUTTON,
+                    Material.PORKCHOP,
+                    Material.RABBIT,
+                    Material.TROPICAL_FISH,
+                    Material.COOKED_BEEF,
+                    Material.COOKED_CHICKEN,
+                    Material.COOKED_MUTTON,
+                    Material.COOKED_PORKCHOP,
+                    Material.COOKED_RABBIT
+            );
+
+            for (Material food : meat) {
+                if (food.equals(event.getItem().getType())) {
+                    if (player.getFoodLevel() == 20) {
+                        player.setFoodLevel(19);
+                    }
+                }
+            }
+        }
+    }
+
+    //mentalPoints below 80
+    @EventHandler
+    public void onPlayerHit(EntityDamageByEntityEvent event){
+        if(event.getDamager() instanceof Player player){
+            if(getPlayerMental(player).getMentalPoints()<=80) {
+                if (event.getEntity() instanceof Creature) {
+                    if (!(event.getEntity() instanceof Monster)) {
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+
+    //mentalPoints 60
+    @EventHandler
+    public void onPlayerFoodLevelRaise(FoodLevelChangeEvent event){
+        if(event.getEntity() instanceof Player player){
+            if(getPlayerMental(player).getMentalPoints()<=60) {
+                event.setFoodLevel(1);
+            }
+        }
+    }
+
+    //mentalPoints 20
+    @EventHandler
+    public void onPlayerBlockBreak(BlockBreakEvent event){
+        if(event.getPlayer() instanceof Player player){
+
+            if(getPlayerMental(player).getMentalPoints()<=20) {
+                List<Material> pickaxes = Arrays.asList(
+                        Material.WOODEN_PICKAXE,
+                        Material.STONE_PICKAXE,
+                        Material.GOLDEN_PICKAXE,
+                        Material.IRON_PICKAXE,
+                        Material.DIAMOND_PICKAXE,
+                        Material.NETHERITE_PICKAXE
+                );
+                for(Material pickaxe : pickaxes){
+                    if(player.getItemInUse().getType()==pickaxe){
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+
+    //mentalPoints 10
+    @EventHandler
+    public void onPlayerTargeted(EntityTargetEvent event){
+        if(event.getTarget() instanceof Player){
+            event.setCancelled(true);
+        }
     }
 }
